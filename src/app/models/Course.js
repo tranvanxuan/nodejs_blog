00d13b9e4d04836bc4ecb108f92d52d2c19c0d3a
tranvanxuan <tranvanxuan96haui@gmail.com>
2022-07-25 07:58:06 +0700

    route detail

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -7,6 +7,7 @@ const Course = new Schema({
     image: { type: String, maxLength: 255 },
     createAt: { type: Date, default: Date.now },
     updateAt: { type: Date, default: Date.now },
+    slug: { type: String, maxLength: 255 },
 });
 
 module.exports = mongoose.model('Course', Course);
